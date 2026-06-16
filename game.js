50619e0eca4eaeede6150eef1d13bf5b847d3d6c
saatvik <129231846+Hea1io@users.noreply.github.com>
2026-06-16 15:08:09 +1000

    2d minecraft v0.1
    tried adding player and movement, lowk failed

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
-const canvas = document.getElementByID("gameCanvas");
+const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const TILE_SIZE = 32;
+const GRAVITY = 0.5;
+const MOVE_SPEED = 4;
+const JUMP_SPEED = -12; 
 
 const world = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -18,8 +21,21 @@ const player = {
     x: 100,
     y: 50,
     width: 25, 
-    height: 40
+    height: 40,
+    vx: 0,
+    vy: 0,
+    onGround: false 
 };
+
+const keys = {};
+
+document.addEventListener("keydown", function(event) {
+    keys[event.key] = true;
+});
+
+document.addEventListener("keyup", function(event) {
+    keys[event.key] = false;
+});
 
 function drawBlock(x, y, type) {
     if (type === 0) return;
@@ -62,18 +78,59 @@ function drawPlayer() {
     );
 }
 
+function updatePlayer() {
+    
+    player.vx = 0;
+
+    if (keys["a"] || keys["ArrowLeft"]) {
+        player.vx = -MOVE_SPEED;
+    }
+
+    if (keys["d"] || keys["ArrowRight"]) {
+        player.vx = MOVE_SPEED;
+    }
+
+    if (
+        (keys["w"] || keys["ArrowUp"]) || keys[" "]) &&
+        player.onGround
+    ) {
+        player.vy = JUMP_SPEED;
+        player.onGround = false;
+    }
+
+    
+
+player.x += player.vx;
+
+player.vy += GRAVITY;
+player.y += player.vy;
+
+const groundY = 4 * TILE_SIZE;
+
+if (player.y + player.height > groundY) {
+    player.y = groundY - player.height;
+    player.vy = 0;
+    player.onGround = true;
+ } 
+}
+
 function draw() {
     
-    ctx.clearRect(0,0 canvas.width, canvas.height);
+    ctx.clearRect(0,0, canvas.width, canvas.height);
 
     drawWorld();
 
     drawPlayer();
 }
 
-draw():
+function gameLoop() {
+    updatePlayer();
+    draw();
+    requestAnimationFrame(gameLoop);
 
+}
 
+gameLoop();
 
 
 
